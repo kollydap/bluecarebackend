621e0fde2408f834883ed4dc39ea414183c33dd4
kollydap <dapkolly@gmail.com>
2023-02-28 13:33:35 +0100

    more commit

--- doctor/serializers.py
+++ doctor/serializers.py
@@ -1,20 +1,23 @@
-
 from rest_framework import serializers
-from .models import DoctorProfile,Education,Experience
+from .models import DoctorProfile, Education, Experience
+
 
 class EducationSerializer(serializers.ModelSerializer):
     class Meta:
-        model=Education
-        fields = '__all__'
-        
-class ExperienceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Experience
+        model = Education
         fields = '__all__'
 
+
+class ExperienceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Experience
+        fields = '__all__'
+
+
 class DoctorSerializer(serializers.ModelSerializer):
-    education = EducationSerializer(read_only = True, many=True)
-    experience = ExperienceSerializer(read_only = True, many=True)
+    education = EducationSerializer(read_only=True, many=True)
+    experience = ExperienceSerializer(read_only=True, many=True)
+
     class Meta:
         model = DoctorProfile
         fields = '__all__'
--- doctor/views.py
+++ doctor/views.py
@@ -3,12 +3,14 @@ from .models import DoctorProfile
 from .serializers import DoctorSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
+
 class DoctorList(generics.ListAPIView):
     queryset = DoctorProfile.objects.all()
     serializer_class = DoctorSerializer
 
+
 class DoctorDetail(generics.RetrieveAPIView):
-   
+
     permission_classes = [IsAuthenticated]
     queryset = DoctorProfile.objects.all()
     serializer_class = DoctorSerializer
